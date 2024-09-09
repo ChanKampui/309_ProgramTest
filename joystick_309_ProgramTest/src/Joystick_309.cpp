dba1a485dc0ac56429dc39e222f1c3bc1ef0f282
Ray <162933772+ChanKampui@users.noreply.github.com>
2024-09-09 17:28:04 +0800

    Update Joystick_309.cpp

--- joystick_309_ProgramTest/src/Joystick_309.cpp
+++ joystick_309_ProgramTest/src/Joystick_309.cpp
@@ -1,5 +1,7 @@
 #include "Joystick_309.h"
 
+// Must Check the function can run successful befor go to next function
+
 Joystick::Joystick(const uint8_t pin_Joystick_X, const uint8_t pin_Joystick_Y){
 
 }
